e75f80c8cea5f6b12a931e5b1afcfcdbad6b29b5
Ethan <ethansikes@letu.edu>
2017-03-01 18:07:40 -0600

    Add Exercise Class & Stubs Add Workout Class & Stubs Add UserAccount Class & Stubs

---
+++ WorkoutTracker/WorkoutTracker/Exercise.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutTracker
+{
+    class Exercise
+    {
+        private String ExerciseName;
+        private String[] Characteristics = new String[4];
+        private String[] Values = new String[4];
+
+        public Exercise()
+        {
+
+        }
+
+        public String getExercise()
+        {
+            return "";
+        }
+
+        private void setExercise(string exercise)
+        {
+
+        }
+
+        public String getChar(int element)
+        {
+            return "";
+        }
+
+        private void setChar(int element, string chara)
+        {
+            
+        }
+
+        public String getVal(int element)
+        {
+            return "";
+        }
+
+        private void setVal(int element, String value)
+        {
+
+        }
+    }
+}
