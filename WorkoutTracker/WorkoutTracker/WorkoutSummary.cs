594fb275afcdd42c845bab5a7d1e2dbb29709a3a
Ethan <ethansikes@live.com>
2017-04-07 17:04:31 -0500

    Got database Inserts working. Routed all user information through the UserAccount class.

--- WorkoutTracker/WorkoutTracker/WorkoutSummary.cs
+++ WorkoutTracker/WorkoutTracker/WorkoutSummary.cs
@@ -14,15 +14,14 @@ namespace WorkoutTracker
 {
     public partial class WorkoutSummary : Form
     {
-        private SqlConnection con;
+        private Exercise headExercise;
+        private UserAccount Account;
 
-        public WorkoutSummary(SqlConnection connection)
-        public WorkoutSummary(Exercise firstExercise)
+        public WorkoutSummary(Exercise firstExercise, UserAccount account)
         {
             InitializeComponent();
-            con = connection;
-            Exercise headExercise = firstExercise;
-
+            headExercise = firstExercise;
+            Account = account;
         }
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
@@ -38,65 +37,6 @@ namespace WorkoutTracker
             //populate the row with labels (http://stackoverflow.com/questions/7170673/how-can-i-create-labels-inside-a-for-loop)
             //if there are values in the char/val fields
             //print the values to their labels
-
-            //SqlDataReader dataReader;
-            //SqlCommand comm;
-            //String workout = "blank";
-            //Exercise headExercise = new Exercise();
-
-            //comm = new SqlCommand("SELECT (Characteristic1, Characteristic2, Characteristic3, Characteristic4, Value1, Value2, Value3, Value4, Name) FROM [Exercises] WHERE Id_Work = @Workout", con);
-            //comm.Parameters.AddWithValue("Workout", workout);
-            //try
-            //{
-            //    dataReader = comm.ExecuteReader();
-
-            //    while (dataReader.Read())
-            //    {
-            //        Exercise temp = new Exercise(dataReader.GetValue(8).ToString());
-            //        temp.setChar(0, dataReader.GetValue(0).ToString());
-            //        temp.setChar(1, dataReader.GetValue(1).ToString());
-            //        temp.setChar(2, dataReader.GetValue(2).ToString());
-            //        temp.setChar(3, dataReader.GetValue(3).ToString());
-            //        temp.setVal(0, dataReader.GetValue(4).ToString());
-            //        temp.setVal(1, dataReader.GetValue(5).ToString());
-            //        temp.setVal(2, dataReader.GetValue(6).ToString());
-            //        temp.setVal(3, dataReader.GetValue(7).ToString());
-            //        headExercise.SetCurrentExercise(temp);
-            //        headExercise.AddToCurrentNext(new Exercise());
-            //        headExercise.MoveNextNode();
-            //    }
-            //}
-            //catch (SqlException f)
-            //{
-            //    ;
-            //}
-        }
-
-        public void SaveWorkout()
-        {
-            SqlDataReader dataReader;
-            SqlCommand comm;
-
-            String workout = "blank";
-            int i = 0;
-            while (true)
-            {
-                String characteristic = "";
-                String value = "";
-                comm = new SqlCommand("INSERT INTO [Exercises] (Id_Work, Characteristic, Value) VALUES (@workout,@Characteristic,Value)", con);
-                comm.Parameters.AddWithValue("Id", i);
-                comm.Parameters.AddWithValue("Id_Work", workout);
-                comm.Parameters.AddWithValue("Characteristic", characteristic);
-                comm.Parameters.AddWithValue("Value", value);
-                try
-                {
-                    comm.ExecuteNonQuery();
-                }
-                catch (SqlException f)
-                {
-                    ;
-                }
-            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -128,10 +68,14 @@ namespace WorkoutTracker
                         //newLine = string.Format("{0},{1}", "Characteristic", "Value");
                         //csv.AppendLine(newLine);
 
-                
-
                 File.WriteAllText(saveFileDialog1.FileName, csv.ToString());
             }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Account.newWorkout("", headExercise);
+            Close();
         }
     }
 }
